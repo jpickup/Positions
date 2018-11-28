afd6906c69d0f36d33f473eb694ea581c1172934
John Pickup <home@johnpickup.com>
2018-11-28 09:44:57 +0000

    Update PositionLadder.java

--- src/com/johnpickup/position/PositionLadder.java
+++ src/com/johnpickup/position/PositionLadder.java
@@ -15,29 +15,61 @@ import java.util.TreeMap;
  */
 @RequiredArgsConstructor
 public class PositionLadder {
-    private final TreeMap<LocalDate, Position> positions = new TreeMap<>();
+    private final PositionKey positionKey;
+    private final ConcurrentSkipListMap<LocalDate, Position> positions = new ConcurrentSkipListMap<>();
+    private final InstrumentDetails instrumentDetails;
     private final List<PositionSubscriber> subscribers;
+    private final Object lock = new Object();
 
     public Position getPosition(LocalDate date) {
         Map.Entry<LocalDate, Position> positionEntry = positions.floorEntry(date);
-        return positionEntry==null?new Position():positionEntry.getValue();
+        return positionEntry == null ? new Position() : positionEntry.getValue();
+    }
+
+    public void setInitialCashPosition(LocalDate date, BigDecimal position) {
+        synchronized (lock) {
+            Position currentPosition = getPosition(date);
+            BigDecimal delta = position.subtract(currentPosition.getPositionComponent(PositionCategory.BOND));
+            applyDelta(date, PositionCategory.BOND, delta);
+        }
     }
 
     public void applyDelta(LocalDate date, PositionCategory category, BigDecimal delta) {
-        positions.put(date, applyDeltaToPosition(getPosition(date), category, delta));
-        NavigableMap<LocalDate, Position> futurePositions = positions.tailMap(date, false);
-        futurePositions.forEach((localDate, position) ->
-                futurePositions.replace(localDate, applyDeltaToPosition(position, category, delta)));
+        synchronized (lock) {
+            positions.put(date, applyDeltaToPosition(date, getPosition(date), category, delta));
+            NavigableMap<LocalDate, Position> futurePositions = positions.tailMap(date, false);
+            futurePositions.forEach((localDate, position) ->
+                    futurePositions.replace(localDate, applyDeltaToPosition(localDate, position, category, delta)));
+
+            if (instrumentDetails != null && !instrumentDetails.isPerpetual() &&
+                    !instrumentDetails.getMaturityDate().isBefore(date)) {
+                positions.put(instrumentDetails.getMaturityDate(), new MaturedBondPosition());
+            }
+        }
     }
 
-    private Position applyDeltaToPosition(Position position, PositionCategory category, BigDecimal delta) {
+    private Position applyDeltaToPosition(LocalDate date, Position position, PositionCategory category, BigDecimal delta) {
         Position newPosition = position.applyDelta(category, delta);
-        notifySubscribers(newPosition);
+        notifySubscribers(date, newPosition);
         return newPosition;
     }
 
-    private void notifySubscribers(Position position) {
-        PositionNotification positionNotification = new PositionNotification(position);
-        subscribers.stream().forEach(s -> s.update(positionNotification));
+    private void notifySubscribers(LocalDate date, Position position) {
+        PositionNotification positionNotification = new PositionNotification(positionKey, date, position);
+        subscribers.forEach(s -> s.update(positionNotification));
+    }
+
+    public Set<LocalDate> dates() {
+        return positions.keySet();
+    }
+
+    public void broadcast() {
+        positions.forEach(this::notifySubscribers);
+    }
+
+    public Collection<PositionNotification> asNotifications() {
+        return positions.entrySet().stream()
+                .map(entry -> new PositionNotification(positionKey, entry.getKey(), entry.getValue()))
+                .collect(Collectors.toList());
     }
 }
